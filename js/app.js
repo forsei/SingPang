1fe8fc024108cceccf81acd0c280e62df6955ee4
Sing Pang <sing_pang@hotmail.com>
2017-04-18 10:08:46 +0200

    new project

--- js/app.js
+++ js/app.js
@@ -71,6 +71,16 @@
 
         var feaProj2 = [
 
+          {
+              name: 'Phoenix Law Firm',
+              imgSrc: 'img/law_firm_thumbnail.jpg',
+              desc1: 'HTML/CSS',
+              desc2: 'ANGULARJS',
+              desc3: 'PHOTOSHOP',
+              role: 'Front-Dev, UI/UX, Graphic Design',
+              link: 'Phoenixlaw.html'
+          },
+
             {
                   name: 'N4G (Redesign)',
                   imgSrc: 'img/N4G_Thumbnail.jpg',
@@ -87,7 +97,7 @@
                     desc1: 'PHOTOSHOP',
                     desc2: '',
                     desc3: '',
-                    role: 'Web Design, UI/UX',
+                    role: 'Front-Dev, Web Design, UI/UX',
                     link: 'goforbroke.html'
 
                 },
@@ -113,15 +123,7 @@
                   },
 
 
-          {
-              name: 'Application Form / Admin',
-              imgSrc: 'img/form_thumbnail.jpg',
-              desc1: 'HTML/CSS',
-              desc2: 'ANGULARJS',
-              desc3: 'PHOTOSHOP',
-              role: 'Front-Dev, Graphic Design',
-              link: 'form-project.html'
-          },
+
           {
               name: 'Portfolio site',
               imgSrc: 'img/Portfoliosite_thumbnail.jpg',
@@ -291,3 +293,14 @@
           //
           //
         //   ];
+
+        // {
+        //     name: 'Application Form / Admin',
+        //     imgSrc: 'img/form_thumbnail.jpg',
+        //     desc1: 'HTML/CSS',
+        //     desc2: 'ANGULARJS',
+        //     desc3: 'PHOTOSHOP',
+        //     role: 'Front-Dev, Graphic Design',
+        //     link: 'form-project.html'
+        // },
+        //
